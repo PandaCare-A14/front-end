c586171059a17f406c97b9878af1f7e6fbf1b15b
freiazulaika <freiazulaika27@gmail.com>
2025-05-25 11:55:52 +0700

    Completed FE for Schedule and Caregiver's Reservation

--- reservasi_caregiver/urls.py
+++ reservasi_caregiver/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import ReservationListView
+from .views import ReservationListView, ApproveReservationView, RejectReservationView, RescheduleReservationView, CaregiverSchedulesView
 
 urlpatterns = [
     path("caregivers/<uuid:caregiver_id>/reservations/", ReservationListView.as_view(), name="reservation_list"),
+    path("reservations/<uuid:reservation_id>/approve/", ApproveReservationView.as_view(), name="approve_reservation"),
+    path("reservations/<uuid:reservation_id>/reject/", RejectReservationView.as_view(), name="reject_reservation"),
+    path("reservations/<uuid:reservation_id>/reschedule/", RescheduleReservationView.as_view(), name="reschedule_reservation"),
+    path('api/caregivers/<uuid:caregiver_id>/schedules', CaregiverSchedulesView.as_view(), name='caregiver_schedules'),
 ]
